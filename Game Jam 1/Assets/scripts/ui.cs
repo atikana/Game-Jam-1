0c89c1182dafa11d4abc2f895acc921a2c116603
atikana <pratana.atikhomkamalasai@mail.utoronto.ca>
2020-09-22 19:15:33 -0400

    added music

---
+++ Game Jam 1/Assets/scripts/ui.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ui : MonoBehaviour
+{
+
+    public GameObject Mainmenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
